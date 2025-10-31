914cb094bda39e056f5a69d6fa2a945e32b536af
Álvaro Stein <alvarostein@hotmail.com.br>
2025-10-31 11:08:14 -0300

    add simple documentation on README

--- main_scenes/rhythm_test.gd
+++ main_scenes/rhythm_test.gd
@@ -4,9 +4,7 @@ extends Node
 @onready var color_rect: ColorRect = $ColorRect
 
 const GRAY = Color.DIM_GRAY
-#const GREEN = Color(0.1, 0.4, 0.1)
 const GREEN = Color.DARK_GREEN
-#const RED = Color(0.4, 0.1, 0.1)
 const RED = Color.DARK_RED
 
 func _ready() -> void:
@@ -28,17 +26,13 @@ func _on_beat_hit(beat, measure_pos) -> void:
 ]
 
 func _on_action_judged(action: StringName, judgement: InputJudge.Judgment, error_ms: int) -> void:
-	# Ignora se a ação não foi a que queremos testar (Espaço)
 	if action != &"space":
 		return
 		
-	# 1. Define a cor com base no julgamento
 	if judgement == InputJudge.Judgment.HIT:
 		color_rect.color = GREEN
-	else: # Deve ser MISS
+	else:
 		color_rect.color = RED
 	
-	# 2. Cria um "flash" para a cor voltar ao preto
-	# Isso faz a cor sumir suavemente em 0.3 segundos.
 	var tween = create_tween()
 	tween.tween_property(color_rect, "color", GRAY, 0.5)
